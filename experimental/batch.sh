87d0d360add27521c2c2b9daeabba452d6dd9a94
7shi <7shi@live.jp>
2025-11-04 17:40:29 +0900

    command-r-35b: 評価を追加

--- experimental/batch.sh
+++ experimental/batch.sh
@@ -36,11 +36,13 @@ CMD6="uv run translate6.py -f French -t Spanish ../examples/finetuning-fr.txt"
 mkdir -p tr-cmp tr-0 tr-1 tr-2 tr4 tr5 tr6
 #process_translation "$CMD -r 0" tr-cmp/gpt-oss-20b-0.txt ollama:hf.co/unsloth/gpt-oss-20b-GGUF
 
-for m in gemma3:4b gemma3n:e4b gemma2:9b gemma3:12b gemma3:27b phi4 command-r7b aya-expanse:8b aya-expanse:32b qwen3:4b qwen3:14b qwen3:30b qwen3:32b llama3.3 llama4:scout gpt-oss:20b gpt-oss:120b mistral-small3.2 mixtral:8x7b mixtral:8x22b; do
+for m in gemma3:4b gemma3n:e4b gemma2:9b gemma3:12b gemma3:27b phi4 command-r7b command-r:35b aya-expanse:8b aya-expanse:32b qwen3:4b qwen3:14b qwen3:30b qwen3:32b llama3.3 llama4:scout gpt-oss:20b gpt-oss:120b mistral-small3.2 mixtral:8x7b mixtral:8x22b; do
     echo "======== $m ========"
     m2=${m/:/-}
     if [[ "$m" == "gemma2:9b" ]]; then
         model_name="ollama:${m}-instruct-q4_K_M"
+    elif [[ "$m" == "command-r:35b" ]]; then
+        model_name="ollama:command-r:35b-08-2024-q4_K_M"
     else
         model_name="ollama:$m"
     fi
